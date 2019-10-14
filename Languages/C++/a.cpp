9e2e1301b21b814046efcb69c553891ddf3551a6
ankichd11 <46399613+ankichd11@users.noreply.github.com>
2019-10-14 22:50:46 +0530

    Add files via upload

---
+++ Languages/C++/a.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+string i;
+cout<<"Enter a text"<<endl;
+cin>>i;
+cout<<i<<endl;
+if(i=="FLAG"){
+	cout<<"ACTF{R3v3rs1ng_1s_E45y}";
+}
+else
+	cout<<"SORRY!! I thought it'll be easy";
+return 0;
+}
